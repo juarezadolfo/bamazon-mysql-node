0efaaec012c74a4ae2a571b26627c404543c63b9
juarezadolfo <juarezadolfo@hotmail.com>
2018-01-08 16:51:58 -0800

    return product info from MYSQL DB works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,44 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
 var inquirer = require ('inquirer');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    queryAllProducts();
+    // queryDanceSongs();
+  });
+  
+  function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+      }
+      console.log("-----------------------------------");
+    });
+  }
+  
+//   function queryDanceSongs() {
+//     var query = connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
+//       for (var i = 0; i < res.length; i++) {
+//         console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+//       }
+//     });
+  
+//     // logs the actual query being run
+//     console.log(query.sql);
+//   }
+  
