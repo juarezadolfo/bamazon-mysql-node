c752d40094320748213b4c83cc94a364de45827c
juarezadolfo <juarezadolfo@hotmail.com>
2018-01-08 18:09:56 -0800

    added inquirer but broken/cleaned up showAllProducts function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,32 +1,75 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
-var inquirer = require ('inquirer');
+var inquirer = require('inquirer');
 
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "password",
     database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    queryAllProducts();
-  });
-  
-  function queryAllProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-      }
-      console.log("-----------------------------------");
+    showAllProducts();
+    startInquirer();
+});
+
+function showAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log('\nItem ID: ' + res[i].item_id + " | " + 'Product Name: ' + res[i].product_name + " | " + 'Department: ' + res[i].department_name + " | " + 'Price: ' + res[i].price.toString() + " | " + 'Quantity In Stock: ' + res[i].stock_quantity.toString());
+        }
+        console.log("-----------------------------------");
     });
-  }
- 
+}
+
+function startInquirer() {
+    // call up inquirer to prompt the user
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the item ID of the product you would like to buy?",
+            name: "product_id"
+        }, {
+            type: "input",
+            message: "How many units of this product would you like to buy?",
+            name: "stock_quantity"
+        }
+    ]).then(function (answers) {
+        updateProduct(answers.item_id, answers.stock_quantity);
+    });
+}
+
+
+
+
+
+
+// create a song in our database
+function createSong(title, artist, genre) {
+    console.log("Inserting a new song...\n");
+    var query = connection.query(
+        "INSERT INTO songs SET ?",
+        {
+            title: title,
+            artist: artist,
+            genre: genre
+        },
+        function (err, res) {
+            console.log(res.affectedRows + " songs inserted!\n");
+            // when we are done call up inquirer again
+            startInquirer();
+        }
+    );
+
+    // logs the actual query being run
+    console.log(query.sql);
+}
